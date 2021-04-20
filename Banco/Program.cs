020d0e1f3922fb286d31987bee81a99086e35abc
Renan <renan.automation@gmail.com>
2021-04-20 18:00:38 -0300

    Acrescentei uma mensagem de Bom Dia

---
+++ Banco/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+
+namespace Banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bom Dia");
+            ContaBancaria conta;
+
+            Console.Write("Entre com um número : ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o Titular da conta : ");
+            string titular = Console.ReadLine();
+
+            Console.Write("Haverá deposito inicial (s/n) ");
+            char resp = char.Parse(Console.ReadLine());
+            if(resp == 's' || resp =='S')
+            {
+                Console.Write("Entre o valor do Deposito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(numero, titular, depositoInicial);
+            }
+            else
+            {
+                conta = new ContaBancaria(numero, titular);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da Conta: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para depósito : ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);
+
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque : ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(quantia);
+
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+
+
+        }
+    }
+}
